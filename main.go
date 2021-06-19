fcd252ea49edd907803737e4d97deebc46371899
Byung Ha Lee <byungha.lee@metronom.com>
2021-06-19 22:55:00 +0200

    Set up Http server

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github/ByungHaLee/nomadcoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from home!")
+}
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Genesis block")
-	chain.AddBlock("Second block")
-	chain.AddBlock("Third block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }

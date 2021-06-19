8d1bd915cf7c91a4fe345f16841d496b1d408d64
Byung Ha Lee <byungha.lee@metronom.com>
2021-06-19 22:23:46 +0200

    Refactor further

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,20 +1,52 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
 }
 
 var b *blockchain
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	blocks := GetBlockchain().blocks
+	if len(blocks) == 0 {
+		return ""
+	}
+	return blocks[len(blocks)-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 func GetBlockchain() *blockchain {
 	if b == nil {
 		b = &blockchain{}
 	}
 	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
 }
--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github/ByungHaLee/nomadcoin/blockchain"
 )
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Genesis block")
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }

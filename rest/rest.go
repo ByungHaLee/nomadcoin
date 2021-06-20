9d4a6a45ff03967a774bfe46cfe70e71568f099c
Byung Ha Lee <byungha.lee@metronom.com>
2021-06-20 23:20:47 +0200

    NewServeMux

---
+++ rest/rest.go
@@ -0,0 +1,79 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"github/ByungHaLee/nomadcoin/blockchain"
+	"github/ByungHaLee/nomadcoin/utils"
+	"log"
+	"net/http"
+)
+
+var port string
+
+type url string
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type urlDescription struct {
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription{
+		{
+			URL:         url("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "GET",
+			Description: "See All Blocks",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "message:string",
+		},
+		{
+			URL:         url("/blocks/{id}"),
+			Method:      "GET",
+			Description: "Get A Block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+func Start(aPort int) {
+	handler := http.NewServeMux()
+	port = fmt.Sprintf(":%d", aPort)
+	handler.HandleFunc("/", documentation)
+	handler.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+}
